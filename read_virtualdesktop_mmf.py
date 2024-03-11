c9a1e07eda9eddfdeb03d4a4b51676d2201e80c5
DenTechs <48604154+DenTechs@users.noreply.github.com>
2024-03-10 22:35:24 -0400

    almost fixed reading issue

--- read_virtualdesktop_mmf.py
+++ read_virtualdesktop_mmf.py
@@ -3,6 +3,7 @@ import struct
 import numpy as np
 import time
 from dataclasses import dataclass
+import math
 
 @dataclass
 class Quaternion:
@@ -58,19 +59,24 @@ def ReadBool():
     return struct.unpack('?', mm.read(1))[0]
 
 def ReadFloat():
+    Align(4)
     return struct.unpack('f', mm.read(4))[0]
 
 def ReadUint32():
+    Align(4)
     return struct.unpack('L', mm.read(4))[0]
     
 def ReadUint64():
+    Align(8)
     return struct.unpack('Q', mm.read(8))[0]
 
 def ReadVector3():
+    Align(4)
     UnpackedPose = struct.unpack("fff", mm.read(12))
     return Vector3(UnpackedPose[0], UnpackedPose[1], UnpackedPose[2])
 
 def ReadPose():
+    Align(4)
     UnpackedPose = struct.unpack("fffffff", mm.read(28))
     return Pose(Quaternion(UnpackedPose[0],UnpackedPose[1],UnpackedPose[2],UnpackedPose[3]), Vector3(UnpackedPose[4], UnpackedPose[5], UnpackedPose[6]))
 
@@ -86,10 +92,34 @@ def ReadBodyJointLocation():
 def ReadSkeletonJoints():
     return SkeletonJoint(Joint=ReadUint32(), ParentJoint=ReadUint32(), Pose=ReadPose())
 
+def Align(alignAmount: int):
+    while mm.tell() % alignAmount != 0:
+        mm.seek(1,1)
 
+def euler_from_quaternion(x, y, z, w):
+        """
+        Convert a quaternion into euler angles (roll, pitch, yaw)
+        roll is rotation around x in radians (counterclockwise)
+        pitch is rotation around y in radians (counterclockwise)
+        yaw is rotation around z in radians (counterclockwise)
+        """
+        t0 = +2.0 * (w * x + y * z)
+        t1 = +1.0 - 2.0 * (x * x + y * y)
+        roll_x = math.atan2(t0, t1)
+     
+        t2 = +2.0 * (w * y - z * x)
+        t2 = +1.0 if t2 > +1.0 else t2
+        t2 = -1.0 if t2 < -1.0 else t2
+        pitch_y = math.asin(t2)
+     
+        t3 = +2.0 * (w * z + x * y)
+        t4 = +1.0 - 2.0 * (y * y + z * z)
+        yaw_z = math.atan2(t3, t4)
+     
+        return roll_x, pitch_y, yaw_z # in radians
 
 while True:
-    mm = mmap.mmap(-1, 9432,"VirtualDesktop.BodyState")
+    mm = mmap.mmap(-1, 9788,"VirtualDesktop.BodyState")
 
     FaceIsValid = ReadBool()
 
@@ -101,7 +131,7 @@ while True:
         ExpressionWeights[i] = ReadFloat()
         i+=1
     
-    ExpressionConfidences = np.empty(2, dtype=float)
+    ExpressionConfidences = np.empty(ConfidenceCount, dtype=float)
     i = 0
     while i < ExpressionConfidences.size:
         ExpressionConfidences[i] = ReadFloat()
@@ -112,6 +142,9 @@ while True:
 
     LeftEyePose = ReadPose()
     RightEyePose = ReadPose()
+
+    # pose = euler_from_quaternion(LeftEyePose.Orientation.x,LeftEyePose.Orientation.y,LeftEyePose.Orientation.z,LeftEyePose.Orientation.w)
+    # print(f"X: {round(pose[0],4)}\nY: {round(pose[1],4)}\nZ: {round(pose[2],4)}\n")
 
     LeftEyeConfidence = ReadFloat()
     RightEyeConfidence = ReadFloat()
@@ -151,12 +184,11 @@ while True:
         i+=1
 
     SkeletonChangedCount = ReadUint32()
-
-    print(LeftHandActive)
-
+    print(SkeletonChangedCount)
+    
     mm.close()
 
-    time.sleep(0.1)
+    time.sleep(0.25)
     #break
 
 
