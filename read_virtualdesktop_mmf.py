5437957b2b8b8435e034cdb7cf953ed1614e9fa4
DenTechs <48604154+DenTechs@users.noreply.github.com>
2024-03-12 21:49:51 -0400

    changed code into functions

--- read_virtualdesktop_mmf.py
+++ read_virtualdesktop_mmf.py
@@ -1,9 +1,8 @@
 import mmap
 import struct
 import numpy as np
-import time
 from dataclasses import dataclass
-import math
+from win32 import win32event
 
 @dataclass
 class Quaternion:
@@ -50,151 +49,128 @@ class SkeletonJoint:
     ParentJoint: np.uint32
     Pose: Pose
 
+MemoryMapName = "VirtualDesktop.BodyState"
+BodyStateEventName = "VirtualDesktop.BodyStateEvent"
+
 ExpressionCount = 70
 ConfidenceCount = 2
 HandJointCount = 26
 FullBodyJointCount = 84
 
-def ReadBool():
+def ReadBool(mm: mmap.mmap):
     return struct.unpack('?', mm.read(1))[0]
 
-def ReadFloat():
-    Align(4)
+def ReadFloat(mm: mmap.mmap):
+    Align(mm, 4)
     return struct.unpack('f', mm.read(4))[0]
 
-def ReadUint32():
-    Align(4)
+def ReadUint32(mm: mmap.mmap):
+    Align(mm, 4)
     return struct.unpack('L', mm.read(4))[0]
     
-def ReadUint64():
-    Align(8)
+def ReadUint64(mm: mmap.mmap):
+    Align(mm, 8)
     return struct.unpack('Q', mm.read(8))[0]
 
-def ReadVector3():
-    Align(4)
+def ReadVector3(mm: mmap.mmap):
+    Align(mm, 4)
     UnpackedPose = struct.unpack("fff", mm.read(12))
     return Vector3(UnpackedPose[0], UnpackedPose[1], UnpackedPose[2])
 
-def ReadPose():
-    Align(4)
+def ReadPose(mm: mmap.mmap):
+    Align(mm, 4)
     UnpackedPose = struct.unpack("fffffff", mm.read(28))
     return Pose(Quaternion(UnpackedPose[0],UnpackedPose[1],UnpackedPose[2],UnpackedPose[3]), Vector3(UnpackedPose[4], UnpackedPose[5], UnpackedPose[6]))
 
-def ReadFingerJointState():
-    return FingerJointState(Pose=ReadPose(), Radius=ReadFloat(), AngularVelocity=ReadVector3(), LinearVelocity=ReadVector3())
+def ReadFingerJointState(mm: mmap.mmap):
+    return FingerJointState(Pose=ReadPose(mm), Radius=ReadFloat(mm), AngularVelocity=ReadVector3(mm), LinearVelocity=ReadVector3(mm))
 
-def ReadHandTrackingAimState():
-    handTrackingAimStateRead = HandTrackingAimState(AimStatus=ReadUint64(), AimPose=ReadPose(), PinchStrengthIndex=ReadFloat(), PinchStrengthMiddle=ReadFloat(), PinchStrengthRing=ReadFloat(), PinchStrengthLittle=ReadFloat())
-    Align(8)
+def ReadHandTrackingAimState(mm: mmap.mmap):
+    handTrackingAimStateRead = HandTrackingAimState(AimStatus=ReadUint64(mm), AimPose=ReadPose(mm), PinchStrengthIndex=ReadFloat(mm), PinchStrengthMiddle=ReadFloat(mm), PinchStrengthRing=ReadFloat(mm), PinchStrengthLittle=ReadFloat(mm))
+    Align(mm, 8)
     return handTrackingAimStateRead
 
-def ReadBodyJointLocation():
-    bodyJointLocationRead = BodyJointLocation(LocationFlags=ReadUint64(), Pose=ReadPose())
-    Align(8)
+def ReadBodyJointLocation(mm: mmap.mmap):
+    bodyJointLocationRead = BodyJointLocation(LocationFlags=ReadUint64(mm), Pose=ReadPose(mm))
+    Align(mm, 8)
     return bodyJointLocationRead
 
-def ReadSkeletonJoints():
-    return SkeletonJoint(Joint=ReadUint32(), ParentJoint=ReadUint32(), Pose=ReadPose())
+def ReadSkeletonJoints(mm: mmap.mmap):
+    return SkeletonJoint(Joint=ReadUint32(mm), ParentJoint=ReadUint32(mm), Pose=ReadPose(mm))
 
-def Align(alignAmount: int):
+def Align(mm: mmap.mmap, alignAmount: int):
     while mm.tell() % alignAmount != 0:
         mm.seek(1,1)
 
-def euler_from_quaternion(x, y, z, w):
-        """
-        Convert a quaternion into euler angles (roll, pitch, yaw)
-        roll is rotation around x in radians (counterclockwise)
-        pitch is rotation around y in radians (counterclockwise)
-        yaw is rotation around z in radians (counterclockwise)
-        """
-        t0 = +2.0 * (w * x + y * z)
-        t1 = +1.0 - 2.0 * (x * x + y * y)
-        roll_x = math.atan2(t0, t1)
-     
-        t2 = +2.0 * (w * y - z * x)
-        t2 = +1.0 if t2 > +1.0 else t2
-        t2 = -1.0 if t2 < -1.0 else t2
-        pitch_y = math.asin(t2)
-     
-        t3 = +2.0 * (w * z + x * y)
-        t4 = +1.0 - 2.0 * (y * y + z * z)
-        yaw_z = math.atan2(t3, t4)
-     
-        return roll_x, pitch_y, yaw_z # in radians
+def AwaitVirtualDesktopSyncEvent(timeout = 1000):
+    event_handle = win32event.OpenEvent(win32event.SYNCHRONIZE, False, BodyStateEventName)
+    win32event.WaitForSingleObject(event_handle,timeout)
 
-while True:
-    mm = mmap.mmap(-1, 9788,"VirtualDesktop.BodyState")
+def GetVirtualDesktopBodyDict():
+    dict = {}
+    mm = mmap.mmap(-1, 9788, MemoryMapName)
 
-    FaceIsValid = ReadBool()
+    dict["FaceIsValid"] = ReadBool(mm)
 
-    IsEyeFollowingBlendshapesValid = ReadBool()
+    dict["IsEyeFollowingBlendshapesValid"] = ReadBool(mm)
 
-    ExpressionWeights = np.empty(ExpressionCount, dtype=float)
+    dict["ExpressionWeights"] = {}
     i = 0
-    while i < ExpressionWeights.size:
-        ExpressionWeights[i] = ReadFloat()
+    while i < ExpressionCount:
+        dict["ExpressionWeights"][i] = ReadFloat(mm)
         i+=1
     
-    ExpressionConfidences = np.empty(ConfidenceCount, dtype=float)
+    dict["ExpressionConfidences"] = {}
     i = 0
-    while i < ExpressionConfidences.size:
-        ExpressionConfidences[i] = ReadFloat()
+    while i < ConfidenceCount:
+        dict["ExpressionConfidences"][i] = ReadFloat(mm)
         i+=1
 
-    LeftEyeIsValid = ReadBool()
-    RightEyeIsValid = ReadBool()
+    dict["LeftEyeIsValid"] = ReadBool(mm)
+    dict["RightEyeIsValid"] = ReadBool(mm)
 
-    LeftEyePose = ReadPose()
-    RightEyePose = ReadPose()
+    dict["LeftEyePose"] = ReadPose(mm)
+    dict["RightEyePose"] = ReadPose(mm)
 
-    # pose = euler_from_quaternion(LeftEyePose.Orientation.x,LeftEyePose.Orientation.y,LeftEyePose.Orientation.z,LeftEyePose.Orientation.w)
-    # print(f"X: {round(pose[0],4)}\nY: {round(pose[1],4)}\nZ: {round(pose[2],4)}\n")
+    dict["LeftEyeConfidence"] = ReadFloat(mm)
+    dict["RightEyeConfidence"] = ReadFloat(mm)
 
-    LeftEyeConfidence = ReadFloat()
-    RightEyeConfidence = ReadFloat()
+    dict["LeftHandActive"] = ReadBool(mm)
+    dict["RightHandActive"] = ReadBool(mm)
 
-    LeftHandActive = ReadBool()
-    RightHandActive = ReadBool()
-
-    LeftHandJointStates = np.empty(HandJointCount, dtype=FingerJointState)
+    dict["LeftHandJointStates"] = {}
     i = 0
-    while i < LeftHandJointStates.size:
-        LeftHandJointStates[i] = ReadFingerJointState()
+    while i < HandJointCount:
+        dict["LeftHandJointStates"][i] = ReadFingerJointState(mm)
         i+=1
 
-    RightHandJointStates = np.empty(HandJointCount, dtype=FingerJointState)
+    dict["RightHandJointStates"] = {}
     i = 0
-    while i < RightHandJointStates.size:
-        RightHandJointStates[i] = ReadFingerJointState()
+    while i < HandJointCount:
+        dict["RightHandJointStates"][i] = ReadFingerJointState(mm)
         i+=1
 
-    LeftAimState = ReadHandTrackingAimState()
-    RightAimState = ReadHandTrackingAimState()
+    dict["LeftAimState"] = ReadHandTrackingAimState(mm)
+    dict["RightAimState"] = ReadHandTrackingAimState(mm)
 
-    BodyTrackingCalibrated = ReadBool()
-    BodyTrackingHighFidelity = ReadBool()
-    BodyTrackingConfidence = ReadFloat()
+    dict["BodyTrackingCalibrated"] = ReadBool(mm)
+    dict["BodyTrackingHighFidelity"] = ReadBool(mm)
+    dict["BodyTrackingConfidence"] = ReadFloat(mm)
 
-    BodyJoints = np.empty(FullBodyJointCount, dtype=BodyJointLocation)
+    dict["BodyJoints"] = {}
     i = 0
-    while i < BodyJoints.size:
-        BodyJoints[i] = ReadBodyJointLocation()
+    while i < FullBodyJointCount:
+        dict["BodyJoints"][i] = ReadBodyJointLocation(mm)
         i+=1
 
-    SkeletonJoints = np.empty(FullBodyJointCount, dtype=SkeletonJoint)
+    dict["SkeletonJoints"] = {}
     i = 0
-    while i < SkeletonJoints.size:
-        SkeletonJoints[i] = ReadSkeletonJoints()
+    while i < FullBodyJointCount:
+        dict["SkeletonJoints"][i] = ReadSkeletonJoints(mm)
         i+=1
 
-    SkeletonChangedCount = ReadUint32()
+    dict["SkeletonChangedCount"] = ReadUint32(mm)
 
     mm.close()
 
-    time.sleep(0.25)
-    #break
-
-
-
-
-
+    return dict
